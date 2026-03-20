64117b09506a5f218e96279b03197b8ef40049b1
sai96032 <venkatasai96032@gmail.com>
2026-03-20 21:39:02 +0530

    move the files to react

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,4 +1,5 @@
-
+import {Routes, Route} from 'react-router';
+import './App.css';
 import { HomePage } from './Pages/HomePage';
 
 
@@ -6,7 +7,10 @@ function App() {
  
 
   return (
-     <HomePage />
+
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    </Routes>     
   )
 }
 
